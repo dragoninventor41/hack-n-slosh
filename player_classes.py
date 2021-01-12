3de6a08ab52c335c915cd4152f70fbc8e3159895
dragoninventor <dragoninventor41@gmail.com>
2021-01-11 22:41:07 -0700

    Fixed image loading for windows

--- player_classes.py
+++ player_classes.py
@@ -5,7 +5,7 @@ class Assassin(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
 
-		self.idle_sprite = pygame.image.load('assets/summoner.jpg').convert() # Currently using prototype summoner image, to be changed later
+		self.idle_sprite = pygame.image.load(f'{path}/assets/summoner.jpg').convert() # Currently using prototype summoner image, to be changed later
 		self.idle_sprite_scaled = pygame.transform.scale(self.idle_sprite, (self.idle_sprite.get_width(), self.idle_sprite.get_height()))
 
 		self.image = self.idle_sprite
